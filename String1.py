1ec0e443189a21210f059b4c5149908250f6e665
Brianyuko <brianyuko45@gmail.com>
2020-03-15 00:30:55 +0700

    dokumentasi string

---
+++ String1.py
@@ -0,0 +1,25 @@
+text1 = 'Jalan - jalan di hari minggu'
+text2 = 'Jalan - jalan di hari jum\'at' #Menggunakan \ untuk ' didalam petik 1
+text3 = "Jalan - jalan di hari jum'at"
+text4 = 'Arip berkata, "Kemarin kemana bro?"'
+text5 = "Nani menjawab, \" ke Bioskop \""
+text6 = 'Arip berkata, "Kemarin kemana bro?" \n Nani menjawab "Ke bioskop"'
+text7 = """
+Arip : "Kemarin kemana bro?"
+Nani : "Ke bioskop"
+"""
+text8 = r'/home/lazyturtle' #raw string
+print(text8)
+print(5*"wk")
+kueA = 'Bolu'
+print('Kue' 'Bolu')
+print('Kue ' +kueA)
+
+#Slicing Text
+text9 = 'Hero 57'
+a = text9[0:4]
+b = text9[0:]
+c = text9[-2]
+print(a)
+print(b)
+print(c)
